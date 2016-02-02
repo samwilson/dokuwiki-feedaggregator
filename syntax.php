ffbce80333ff94c995fdb13df69356f3ba1f0c71
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:09:34 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -58,7 +58,7 @@ class syntax_plugin_feedaggregator extends DokuWiki_Syntax_Plugin {
      * @param Doku_Handler    $handler The handler
      * @return array Data for the renderer
      */
-    public function handle($match, $state, $pos, Doku_Handler &$handler) {
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
         global $conf;
         $data = array();
         // Are we to handle this match? If not, don't.
@@ -97,7 +97,7 @@ class syntax_plugin_feedaggregator extends DokuWiki_Syntax_Plugin {
      * @param array          $data      The data from the handler() function
      * @return bool If rendering was successful.
      */
-    public function render($mode, Doku_Renderer &$renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         if ($mode != 'xhtml') {
             return false;
         }
